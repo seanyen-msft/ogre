942b6b4e0848b30c784dccaa75c13edd9fcb67ac
Sean Yen [MSFT] <seanyen@microsoft.com>
2018-08-15 09:24:52 -0700

    Update ci-build.cmake

--- ci-build.cmake
+++ ci-build.cmake
@@ -38,7 +38,7 @@ endif()
 
 if(DEFINED ENV{APPVEYOR})
     set(CMAKE_BUILD_TYPE RelWithDebInfo)
-    set(GENERATOR -G "Visual Studio 15")
+    set(GENERATOR -G "Ninja")
     set(RENDERSYSTEMS
         -DOGRE_BUILD_RENDERSYSTEM_D3D9=FALSE
         -DOGRE_BUILD_RENDERSYSTEM_GL=TRUE
